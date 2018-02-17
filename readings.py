098409a79287be7b34129010e16d1ad94f8958d1
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2018-02-18 03:03:18 +0530

    Add files via upload

---
+++ readings.py
@@ -0,0 +1,35 @@
+import numpy as np 
+import matplotlib 
+
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt 
+import time
+
+F = [[]for i in range(8)]
+T = [[]for i in range(8)]
+
+for i in range(8) :
+	print('CORE-'+str(i+1)+'\n')
+	duration = 0.5
+	start = time.time()
+	while time.time() < start + duration :
+		f = np.loadtxt('/sys/devices/system/cpu/cpu'+str(i)+'/cpufreq/scaling_cur_freq')
+		t = np.loadtxt('/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp'+str(i+1)+'_input')
+		x = (f.astype(int)).tolist()
+		y = (t.astype(int)).tolist()
+		print(str(x)+'   --->  '+str(y))
+		F[i].append(x/1000)
+		T[i].append(y/1000)
+		time.sleep(0.001)
+	print('--------------------------------------------------------------------\n')	
+
+for i in range(8) :
+	plt.plot(F[i],T[i],label = 'Core-'+str(i+1))
+
+plt.xlabel('frequency(1:1000)')
+plt.ylabel('temperature(1:1000)')
+plt.legend()
+
+plt.savefig('Cores.png')
+
